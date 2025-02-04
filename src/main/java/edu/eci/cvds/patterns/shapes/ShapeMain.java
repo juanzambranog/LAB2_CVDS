2d1d364550697740be8fa0be1288b413729901ea
AllanContreras <allan.contreras-r@mail.escuelaing.edu.co>
2025-02-04 18:54:05 -0500

    Update ShapeMain.java

--- src/main/java/edu/eci/cvds/patterns/shapes/ShapeMain.java
+++ src/main/java/edu/eci/cvds/patterns/shapes/ShapeMain.java
@@ -8,15 +8,15 @@ public class ShapeMain {
       return;
     }
     try {
-      //RegularShapeType type = RegularShapeType.valueOf(args[0]);
-      //Shape shape = ShapeFactory.create(type);
-      //System.out.println(
-        //String.format(
-          //"Successfully created a %s with %s sides.",
-          //type,
-          //shape.getNumberOfEdges()
-        //)
-      //);
+      RegularShapeType type = RegularShapeType.valueOf(args[0]);
+      Shape shape = ShapeFactory.create(type);
+      System.out.println(
+        String.format(
+          "Successfully created a %s with %s sides.",
+          type,
+          shape.getNumberOfEdges()
+        )
+      );
     } catch (IllegalArgumentException ex) {
       System.err.println(
         "Parameter '" + args[0] + "' is not a valid RegularShapeType"
